346e9fca2a083bdf20f6b7f8d423cc05fe5f6561
kappybar <newton7233@outlook.jp>
2022-09-01 13:48:57 +0900

    add diff detection

--- reactoid.js
+++ reactoid.js
@@ -38,15 +38,53 @@ function createDom(fiber) {
         fiber.type === "TEXT_ELEMENT" 
         ? document.createTextNode("")
         : document.createElement(fiber.type)
-
-    const isProperty = key => key !== "children"
-    Object.keys(fiber.props)
-        .filter(isProperty)
-        .forEach(name => {
-            dom[name] = fiber.props[name]
-        })   
+    
+    updateDom(dom, {}, fiber.props)  
     
     return dom
+}
+
+const isEvent = key => key.startsWith("on")
+const isProperty = key => key !== "children" && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+    
+    // old propertiesを消す
+    Object.keys(prevProps)
+        .filter(isProperty)
+        .filter(isGone(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = ""
+        })
+
+    // new propertyを付け足す
+    Object.keys(nextProps)
+       .filter(isProperty)
+       .filter(isNew(prevProps, nextProps))
+       .forEach(name => {
+        dom[name] = nextProps[name]
+       })
+    
+    // old event listner を消す
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(
+            key => !(key in nextProps) || isNew(prevProps, nextProps)(key)
+        )
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.removeEventListener(eventType, prevProps[name])
+        })
+
+    // new event listner を足す
+    Object.keys(nextProps)
+        .filter(isEvent)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.addEventListener(eventType, nextProps[name])
+        })
 }
 
 // element(Reactoid要素)をcontainerの要素の下にレンダリングする。
@@ -56,19 +94,28 @@ function render(element, container) {
         props: {
             children: [element],
         },
+        alternate: currentRoot,
     }
+    deletions = []
     nextUnitOfWork = wipRoot
 }
 
-// 今のroot
+// 今、表示されているDom
+let currentRoot = null
+
+// 今、作業中のroot
 let wipRoot = null
 
 // 次の作業単位
 let nextUnitOfWork = null
 
+// 削除するノードを保管する配列
+let deletions = null
+
 // commitフェーズを実行する関数
 function commmitRoot() {
     commitWork(wipRoot.child)
+    currentRoot = wipRoot
     wipRoot = null
 }
 
@@ -78,10 +125,18 @@ function commitWork(fiber) {
         return
     }
     const domParent = fiber.parent.dom
-    domParent.appendChild(fiber.dom)
+    if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+        domParent.appendChild(fiber.dom)
+    } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+        updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+    } else if (fiber.effectTag === "DELETION") {
+        domParent.removeChild(fiber.dom)
+    }
+
     commitWork(fiber.child)
     commitWork(fiber.sibling)
 }
+
 
 function workLoop(deadline) {
     let shouldYield = false
@@ -101,6 +156,60 @@ function workLoop(deadline) {
 }
 requestIdleCallback(workLoop)
 
+function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    let prevSibling = null
+
+    while (index < elements.length || oldFiber != null) {
+        const element = elements[index]
+        let newFiber = null
+
+        const sameType = oldFiber && element && element.type == oldFiber.type
+
+        if (sameType) {
+            // 更新
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,
+                dom: oldFiber.dom,
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: "UPDATE",
+            }
+        }
+        if (element && !sameType) {
+            // 追加
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,
+                parent: wipFiber,
+                alternate: null,
+                effectTag: "PLACEMENT",
+            }
+        }
+        if (oldFiber && !sameType) {
+            // 削除
+            oldFiber.effectTag = "DELETION"
+            deletions.push(oldFiber)
+        }
+
+        if (oldFiber) {
+            oldFiber = oldFiber.sibling
+        }
+
+        if (index === 0) {
+            wipFiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
+    }
+}
+
 function performUnitOfWork(fiber) {
     // DOMのノードを新しく作る
     if (!fiber.dom) {
@@ -109,28 +218,7 @@ function performUnitOfWork(fiber) {
 
     // 次のファイバーを作る
     const elements = fiber.props.children
-    let index = 0
-    let prevSibling = null
-
-    while (index < elements.length) {
-        const element = elements[index]
-
-        const newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null,
-        }
-
-        if (index === 0) {
-            fiber.child = newFiber
-        } else {
-            prevSibling.sibling = newFiber
-        }
-
-        prevSibling = newFiber
-        index++
-    }
+    reconcileChildren(fiber, elements)
 
     // 次の作業単位を返す
     if (fiber.child) {
