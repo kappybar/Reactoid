2ffb2022da3322bf341575fb2c8ede58bb3cd299
kappybar <newton7233@outlook.jp>
2022-09-02 18:20:56 +0900

    add function component

--- reactoid.js
+++ reactoid.js
@@ -99,17 +99,31 @@ function commitWork(fiber) {
     if (!fiber) {
         return
     }
-    const domParent = fiber.parent.dom
+
+    let domParentFiber = fiber.parent
+    while (!domParentFiber.dom) {
+        domParentFiber = domParentFiber.parent
+    }
+    const domParent = domParentFiber.dom
+
     if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
         domParent.appendChild(fiber.dom)
     } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
         updateDom(fiber.dom, fiber.alternate.props, fiber.props)
     } else if (fiber.effectTag === "DELETION") {
-        domParent.removeChild(fiber.dom)
+        commitDeletion(fiber, domParent)
     }
 
     commitWork(fiber.child)
     commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent) {
+    if (fiber.dom) {
+        domParent.removeChild(fiber.dom)
+    } else {
+        commitDeletion(fiber.child, domParent)
+    }
 }
 
 // element(Reactoid要素)をcontainerの要素の下にレンダリングする。
@@ -156,14 +170,12 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
-    // DOMのノードを新しく作る
-    if (!fiber.dom) {
-        fiber.dom = createDom(fiber)
+    const isFunctionComponent = fiber.type instanceof Function
+    if (isFunctionComponent) {
+        updateFunctionComponent(fiber)
+    } else {
+        updateHostComponent(fiber)
     }
-
-    // 次のファイバーを作る
-    const elements = fiber.props.children
-    reconcileChildren(fiber, elements)
 
     // 次の作業単位を返す
     if (fiber.child) {
@@ -178,6 +190,24 @@ function performUnitOfWork(fiber) {
         nextFiber = nextFiber.parent
     }
 
+}
+
+function updateFunctionComponent(fiber) {
+    // fiber.typeが関数コンポーネントになっているからそれにpropsを適用してReactoid要素を得る。
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber, children)
+}
+
+
+function updateHostComponent(fiber) {
+    // DOMのノードを新しく作る
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+
+    // 次のファイバーを作る
+    const elements = fiber.props.children
+    reconcileChildren(fiber, elements)
 }
 
 function reconcileChildren(wipFiber, elements) {
